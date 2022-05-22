1962cfba09e51a5d9044accfc8447447e2de1e02
root <djsra@yandex.ru>
2022-05-22 15:00:00 +0000

    scam script 22/05/2022 ealier then repo

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-date --set="Sat 20 May 2023 03:00:00 PM UTC"
+date --set="Sat 22 May 2022 03:00:00 PM UTC"
 git add .
-git commit -m "scam script 20/05/2023"
+git commit -m "scam script 22/05/2022 ealier then repo"
 date
 git push -u origin main
